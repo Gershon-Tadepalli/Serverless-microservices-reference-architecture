16a08155161b37436a8d66d27c3a4ac52d166ba7
Gershon Tadepalli <gershon.tadepalli@toyota.com>
2020-08-20 00:17:57 +0530

    Added new Settings file to Static website

---
+++ web/serverless-microservices-web/public/js/settings.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '2739e8b5-13d3-4894-b710-544a80e0d255'
+window.authAuthority = 'https://contoso01239Tenant.b2clogin.com/tfp/contoso01239Tenant.onmicrosoft.com/b2c_1_default-signin/v2.0'
+window.authScopes = ['https://contoso01239Tenant.onmicrosoft.com/api/rideshare']
+window.authEnabled = true
+
+// API endpoints
+window.apiKey = 'e86683f42cbb4c28978e4ceb1112faf2'
+window.apiBaseUrl = 'https://rideshare-ger.azure-api.net'
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`
+window.signalrInfoUrl = 'https://ridesharetripsfunctionapp-ger.azurewebsites.net/api/signalrinfo?clientId=apim-rideshare-ger'
